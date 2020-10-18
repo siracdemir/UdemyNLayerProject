b550012891d1216be7a9efd6086e9f5cf161810c
siracdmr <siracdmr@gmail.com>
2020-10-18 11:47:09 +0300

    setting up core layer

---
+++ UdemyNLayerProject.Core/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ UdemyNLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Category { get; }
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
